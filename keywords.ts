8038c02299b94e5f487ee888825b9501f8a4badb
SwathiKrishna27 <92883111+SwathiKrishna27@users.noreply.github.com>
2021-10-29 20:26:06 +0530

    Add files via upload

---
+++ keywords.ts
@@ -0,0 +1,30 @@
+//const keyword
+
+const a=5, b=2, c="swathi";
+
+console.log(a+b);
+console.log(c);
+console.log(a,b,c, a+b+c)
+
+//let keyword
+let _a;
+_a = "swathi";
+console.log(_a)
+
+let a1, b1, c1;
+
+a1=3;
+console.log(a1,b1,c1);
+
+// var keyword
+
+var x,y;
+x="s", y= "wathi" z=1;
+console.log(x + y + z);
+
+
+// == operator
+var i, j; // declaring variables
+i =10, j='10'; // assigning values to varables
+console.log(i==j)
+console.log(i===j)
