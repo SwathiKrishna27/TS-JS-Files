e7b77a92f4bfad2f8738dc053101dfe30f6df64c
SwathiKrishna27 <92883111+SwathiKrishna27@users.noreply.github.com>
2021-11-29 16:39:31 +0530

    Delete fib.js

--- fib.js
+++
@@ -1,9 +0,0 @@
-
-let num1 = 0, num2 = 1, nextnum; //declaring variables 
-console.log('Fibonacci Series:');
-for (let i = 1; i <= 100; i++) {
-    console.log(num1); 
-    nextnum = num1 + num2;
-    num1 = num2;
-    num2 = nextnum;
-}
